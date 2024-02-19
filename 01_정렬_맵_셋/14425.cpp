f89dd021d2f32512f33fd1ed82108d729d083a36
lumiere-on <mjw0214@ewhain.net>
2024-02-19 23:57:50 +0900

    [정렬, 맵, 셋

---
+++ 01_정렬_맵_셋/14425.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <map>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+    int n,m;
+    string input;
+
+    cin >> n >> m;
+
+    int check=0;
+    map<string, int> prepare;
+
+    while(n--){
+        cin >> input;
+        prepare[input]=1;
+    }
+    while(m--){
+        cin >> input;
+        if(prepare[input]==1){
+            check++;
+        }
+        
+    }
+
+    cout << check;
+
+    return 0;
+    
+}
